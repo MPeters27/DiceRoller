bd0fa91ff1f34a83167944d3696f93c7daf06cf4
MPeters27 <mapeters@mines.edu>
2018-11-18 18:52:49 -0700

    diceRoller project complete

--- diceRoller.py
+++ diceRoller.py
@@ -1,15 +1,26 @@
 import random
 
 def rNumberGenerator():
-
     minimum = 1
     maximum = 6
-
     number = random.randint(minimum, maximum)
-
     return number
 
 
-num = rNumberGenerator()
-print(num)
-print("Would you like to play again?")
+def diceRollText():
+    print("Would you like to roll again?")
+    print("Y == roll again")
+    print("N == quit")
+
+
+answer = "Y"
+while answer.upper() == "Y":
+    num = rNumberGenerator()
+    print("You rolled a " + str(num))
+    diceRollText()
+    answer = input()
+    while answer.upper() != "Y" and answer.upper() != "N":
+        print("Invalid entry")
+        diceRollText()
+        answer = input()
+
